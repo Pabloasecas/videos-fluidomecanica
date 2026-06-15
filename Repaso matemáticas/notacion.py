3c40a2a4b28972362f72943129a6796610e8055b
Pablo <pablogcg05@gmail.com>
2026-06-15 21:09:42 +0200

    Animación producto escalar completa

--- Repaso matemáticas/notacion.py
+++ Repaso matemáticas/notacion.py
@@ -1,6 +1,7 @@
 from manim import *
 naranja="#FF5900"
 azul="#00AEEF"
+verde="#27F5B0"
 
 class latex(Scene):
     def construct(self):
@@ -56,10 +57,10 @@ class producto_escalar(Scene):
         self.wait(2)
 
         axes = Axes(
-            x_range=[-1, 4, 1],
+            x_range=[-1, 6, 1],
             y_range=[-1, 4, 1],
-            x_length=6,
-            y_length=6,
+            x_length=7,
+            y_length=5,
             axis_config={"color": azul, "include_numbers": True},
             tips=True,
         )
@@ -86,9 +87,59 @@ class producto_escalar(Scene):
         label_v = MathTex(r"\mathbf{v}", color=naranja).next_to(
             vector_v.get_end(), UP
         )
+        perpendicular_line = Line(
+            axes.c2p(7 / 3, -1),
+            axes.c2p(2 / 3, 4),
+            color=WHITE,
+        )
+        projection_vector = Arrow(
+            axes.c2p(0, 0),
+            axes.c2p(9 / 5, 3 / 5),
+            buff=0,
+            color=verde,
+        )
+        scaled_projection_vector = Arrow(
+            axes.c2p(0, 0),
+            axes.c2p(9 * np.sqrt(10) / 5, 3 * np.sqrt(10) / 5),
+            buff=0,
+            color=verde,
+        )
+        label_w = MathTex(r"\mathbf{w}", color=verde).next_to(
+            scaled_projection_vector.get_end(), UP
+        )
+        product_equality = MathTex(
+            r"\lVert\mathbf{w}\rVert",
+            r"=",
+            r"\mathbf{u}\cdot\mathbf{v}",
+            font_size=48,
+        ).to_edge(RIGHT)
+        product_equality[0].set_color(verde)
+        product_equality[2].set_color(naranja)
 
         self.play(FadeOut(prod_esc2))
         self.play(Write(axes), Write(axes_labels))
         self.play(GrowArrow(vector_u), GrowArrow(vector_v))
         self.play(Write(label_u), Write(label_v))
+        self.wait(2)
+        self.play(Create(perpendicular_line))
+        self.wait(2)
+        self.play(GrowArrow(projection_vector))
+        self.wait(2)
+        self.play(Transform(projection_vector, scaled_projection_vector))
+        self.play(Write(label_w))
+        self.wait(2)
+
+        graph = VGroup(
+            axes,
+            axes_labels,
+            vector_u,
+            vector_v,
+            label_u,
+            label_v,
+            perpendicular_line,
+            projection_vector,
+            label_w,
+        )
+        self.play(graph.animate.shift(LEFT * 2))
+        self.play(Write(product_equality))
         self.wait(2)
